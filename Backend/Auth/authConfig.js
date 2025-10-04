fb112ec6b4d6fd6843c1b7a76e48ccfe0035287c
yatinsingh2007 <yatin4591@gmail.com>
2025-10-04 22:35:37 +0530

    stored token in cookies

--- Backend/Auth/authConfig.js
+++ Backend/Auth/authConfig.js
@@ -12,25 +12,22 @@ async function signup(req, res) {
     const { name, email, password } = req.body;
     if (!name || !email || !password) return res.status(400).json({ message: 'All fields required' });
 
-    // Check if user exists
     const existingUser = await prisma.user.findUnique({ where: { email } });
     if (existingUser) return res.status(400).json({ message: 'User already exists' });
 
-    // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create user
     const user = await prisma.user.create({
       data: { name, email, password: hashedPassword },
     });
 
-    return res.status(201).json({ message: 'User registered successfully', userId: user.id });
+    return res.status(201).json({ message: 'User registered successfully' });
   } catch (err) {
     return res.status(500).json({ message: 'Server error', error: err.message });
   }
 }
 
-// Login function
+
 async function login(req, res) {
   try {
     const { email, password } = req.body;
@@ -42,8 +39,11 @@ async function login(req, res) {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
 
-    const token = jwt.sign({ email: user.email, name: user.name }, JWT_SECRET, { expiresIn: '1h' });
-    return res.status(200).json({ message: 'Login successful', token });
+    const token = jwt.sign({ email: user.email, name: user.name }, JWT_SECRET, { expiresIn: '7h' });
+    return res
+    .status(200)
+    .json({ message: 'Login successful' })
+    .cookie("token" , token);
   } catch (err) {
     return res.status(500).json({ message: 'Server error', error: err.message });
   }
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser')
 const authRouter = require('./Router/authRouter');
 const cors = require('cors')
 
 app.use(express.json());
 app.use(cors())
+app.use(cookieParser())
 
 // Auth routes
 app.use('/auth', authRouter);
